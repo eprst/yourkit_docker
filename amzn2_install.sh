c443800899f6914520118e8ae95310f4a9a6276d
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2018-10-01 04:07:15 +0000

    removed -d from docker run in amzn2_install.sh

--- amzn2_install.sh
+++ amzn2_install.sh
@@ -4,4 +4,4 @@ sudo yum -y install docker
 sudo service docker start
 sudo usermod -a -G docker $USER
 sudo su - $USER # get new group
-docker run -d -p 5901:5901 -p 6901:6901 ksobolev/yourkit
+docker run -p 5901:5901 -p 6901:6901 ksobolev/yourkit
